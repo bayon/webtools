6ab8f27db37cc7253ae7dfedde5abf2ac679813c
Bayon Forte <bayon@forteworks.com>
2014-05-09 08:03:27 -0400

    been a while need to commit.

---
+++ WebTools/dev/layouts/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html >
+	<!-- <html manifest="appcache.appcache"> -->
+	<head>
+		<meta charset="UTF-8">
+		<title>webtools: layouts</title>
+		
+		<!-- <rwd> -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<link rel="stylesheet" type="text/css" href="layouts.css">
+		<!-- </rwd> -->
+
+	</head>
+	
+	<body>
+		<div id = "page">
+			<div id ="section_main"></div>
+			<div id ="section_sub1"></div>
+			
+		</div>
+	</body>
+</html>
